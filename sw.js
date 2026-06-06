a4823650fabd71334deacb20a4f9d4e9ecda157b
marc0130 <marc0130@gmail.com>
2026-06-06 07:45:23 -0700

    update sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'prostrack-cache-v7.7.0';
+const urlsToCache = [
+  'index.html',
+  'manifest.json',
+  'sw.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
